3a360e1d17118f2846c74bcf4f693a3e49434166
Jean Eric Gagnon <jean.eric.gagnon619@gmail.com>
2026-04-16 09:44:57 -0700

    Use builder manifests for section variant lists

--- src/builder/registry/sectionRendererRegistry.ts
+++ src/builder/registry/sectionRendererRegistry.ts
@@ -1,10 +1,11 @@
 import { BuilderSectionType } from '../../types/builder/section';
-import { getSectionComponent, getSectionVariants, SectionComponent } from '../../sections/sectionRegistry';
+import { getSectionComponent, SectionComponent } from '../../sections/sectionRegistry';
+import { SECTION_MANIFESTS } from './sectionManifests';
 
 export function getSectionRenderer(type: BuilderSectionType, variant = 'default'): SectionComponent {
   return getSectionComponent(type, variant);
 }
 
 export function getSectionVariantsList(type: BuilderSectionType): string[] {
-  return getSectionVariants(type);
+  return SECTION_MANIFESTS[type]?.supportedVariants ?? ['default'];
 }
