3d373e5fc75343ac36b24b0e4e6f506d974376a6
Jean Eric Gagnon <jean.eric.gagnon619@gmail.com>
2026-04-21 19:58:54 -0700

    Cover builder hash publishNow fallback truth

--- src/builder/utils/publishUiHints.test.ts
+++ src/builder/utils/publishUiHints.test.ts
@@ -96,6 +96,7 @@ describe('publishUiHints', () => {
     expect(shouldAutoPublishFromSearch('?publishNow=1&foo=bar')).toBe(true);
     expect(shouldAutoPublishFromSearch('?publishNow=true')).toBe(false);
     expect(shouldAutoPublishFromSearch('?publishNow=01')).toBe(false);
+    expect(shouldAutoPublishFromSearch('?publishNow=1#ignored')).toBe(false);
     expect(shouldAutoPublishFromSearch('')).toBe(false);
   });
 
